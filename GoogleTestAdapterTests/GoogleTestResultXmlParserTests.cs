d2558db130d1275c8f268ce9542cf80df8f86855
Christian Soltenborn <christian@soltenborn.de>
2015-08-27 21:54:03 +0200

    More XML tests

--- GoogleTestAdapterTests/GoogleTestResultXmlParserTests.cs
+++ GoogleTestAdapterTests/GoogleTestResultXmlParserTests.cs
@@ -2,7 +2,6 @@
 using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -25,7 +24,7 @@ namespace GoogleTestAdapter
             GoogleTestResultXmlParser Parser = new GoogleTestResultXmlParser("somefile", TestCases, MockLogger.Object);
             List<TestResult> Results = Parser.GetTestResults();
 
-            Assert.IsTrue(Results.Count == 0);
+            Assert.AreEqual(0, Results.Count);
             MockLogger.Verify(L => L.SendMessage(It.Is<TestMessageLevel>(Tml => Tml == TestMessageLevel.Warning), It.IsAny<string>()),
                             Times.Exactly(1));
         }
@@ -39,13 +38,13 @@ namespace GoogleTestAdapter
             GoogleTestResultXmlParser Parser = new GoogleTestResultXmlParser(XML_FILE_BROKEN, TestCases, MockLogger.Object);
             List<TestResult> Results = Parser.GetTestResults();
 
-            Assert.IsTrue(Results.Count == 0);
+            Assert.AreEqual(0, Results.Count);
             MockLogger.Verify(L => L.SendMessage(It.Is<TestMessageLevel>(Tml => Tml == TestMessageLevel.Warning), It.IsAny<string>()),
                             Times.Exactly(1));
         }
 
         [TestMethod]
-        public void FindsSuccessfulResultsFromSample1()
+        public void FindsSuccessfulResultsInSample1()
         {
             string[] Tests = new string[] { "GoogleTestSuiteName1.TestMethod_001" };
             IEnumerable<TestCase> TestCases = Tests.Select(ToTestCase);
@@ -54,11 +53,11 @@ namespace GoogleTestAdapter
             List<TestResult> Results = Parser.GetTestResults();
 
             Assert.AreEqual(1, Results.Count);
-            Assert.IsTrue(Results[0].Outcome == TestOutcome.Passed);
+            AssertTestResultIsPassed(Results[0]);
         }
 
         [TestMethod]
-        public void FindsSuccessfulParameterizedResultFromSample1()
+        public void FindsSuccessfulParameterizedResultInSample1()
         {
             string[] Tests = new string[] { "ParameterizedTestsTest1/AllEnabledTest.TestInstance/7  # GetParam() = (false, 200, 0)" };
             IEnumerable<TestCase> TestCases = Tests.Select(ToTestCase);
@@ -67,8 +66,90 @@ namespace GoogleTestAdapter
             List<TestResult> Results = Parser.GetTestResults();
 
             Assert.AreEqual(1, Results.Count);
-            Assert.IsTrue(Results[0].Outcome == TestOutcome.Passed);
-            Assert.IsTrue(Results[0].ErrorMessage == null);
+            AssertTestResultIsPassed(Results[0]);
+        }
+
+        [TestMethod]
+        public void FindsFailureResultInSample1()
+        {
+            string[] Tests = new string[] { "AnimalsTest.testGetEnoughAnimals" };
+            IEnumerable<TestCase> TestCases = Tests.Select(ToTestCase);
+
+            GoogleTestResultXmlParser Parser = new GoogleTestResultXmlParser(XML_FILE_1, TestCases, MockLogger.Object);
+            List<TestResult> Results = Parser.GetTestResults();
+
+            Assert.AreEqual(1, Results.Count);
+            string ErrorMsg = @"x:\prod\company\util\util.cpp:67
+Value of: animals.size()
+  Actual: 1
+Expected: 3
+Should get three animals";
+            AssertTestResultIsFailure(Results[0], ErrorMsg);
+        }
+
+        [TestMethod]
+        public void FindsParamterizedFailureResultInSample1()
+        {
+            string[] Tests = new string[] { "ParameterizedTestsTest1/AllEnabledTest.TestInstance/11  # GetParam() = (true, 0, 100)" };
+            IEnumerable<TestCase> TestCases = Tests.Select(ToTestCase);
+
+            GoogleTestResultXmlParser Parser = new GoogleTestResultXmlParser(XML_FILE_1, TestCases, MockLogger.Object);
+            List<TestResult> Results = Parser.GetTestResults();
+
+            Assert.AreEqual(1, Results.Count);
+            string ErrorMsg = @"someSimpleParameterizedTest.cpp:61
+Expected: (0) != ((pGSD->g_outputs64[(g_nOutput[ 8 ]-1)/64] & g_dnOutput[g_nOutput[ 8 ]])), actual: 0 vs 0";
+            AssertTestResultIsFailure(Results[0], ErrorMsg);
+        }
+
+        [TestMethod]
+        public void FindsSuccessfulResultInSample2()
+        {
+            string[] Tests = new string[] { "FooTest.DoesXyz" };
+            IEnumerable<TestCase> TestCases = Tests.Select(ToTestCase);
+
+            GoogleTestResultXmlParser Parser = new GoogleTestResultXmlParser(XML_FILE_2, TestCases, MockLogger.Object);
+            List<TestResult> Results = Parser.GetTestResults();
+
+            Assert.AreEqual(1, Results.Count);
+            AssertTestResultIsPassed(Results[0]);
+        }
+
+        [TestMethod]
+        public void FindsFailureResultInSample2()
+        {
+            string[] Tests = new string[] { "FooTest.MethodBarDoesAbc" };
+            IEnumerable<TestCase> TestCases = Tests.Select(ToTestCase);
+
+            GoogleTestResultXmlParser Parser = new GoogleTestResultXmlParser(XML_FILE_2, TestCases, MockLogger.Object);
+            List<TestResult> Results = Parser.GetTestResults();
+
+            Assert.AreEqual(1, Results.Count);
+            string ErrorMsg = @"c:\prod\gtest-1.7.0\staticallylinkedgoogletests\main.cpp:40
+Value of: output_filepath
+  Actual: ""this/package/testdata/myoutputfile.dat""
+Expected: input_filepath
+Which is: ""this/package/testdata/myinputfile.dat""
+Something's not right!!
+
+c:\prod\gtest-1.7.0\staticallylinkedgoogletests\main.cpp:41
+Value of: 56456
+Expected: 12312
+Something's wrong :(";
+            AssertTestResultIsFailure(Results[0], ErrorMsg);
+        }
+
+        private void AssertTestResultIsPassed(TestResult testResult)
+        {
+            Assert.AreEqual(TestOutcome.Passed, testResult.Outcome);
+            Assert.IsNull(testResult.ErrorMessage);
+        }
+
+        private void AssertTestResultIsFailure(TestResult testResult, string errorMessage)
+        {
+            Assert.AreEqual(TestOutcome.Failed, testResult.Outcome);
+            Assert.IsNotNull(testResult.ErrorMessage);
+            Assert.AreEqual(errorMessage.Replace("\r\n", "\n"), testResult.ErrorMessage.Replace("\r\n", "\n"));
         }
 
     }
