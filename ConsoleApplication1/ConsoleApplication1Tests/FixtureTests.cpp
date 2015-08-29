ea893d748c8a8fab8df3592f85242a690c9e2159
Christian Soltenborn <christian@soltenborn.de>
2015-08-29 11:38:01 +0200

    added traits support for simple tests and tests with fixtures

---
+++ ConsoleApplication1/ConsoleApplication1Tests/FixtureTests.cpp
@@ -0,0 +1,41 @@
+#include "gtest/gtest.h"
+#include "../ConsoleApplication1/ConsoleApplication1.h"
+#include "../../GoogleTestExtension/GTA_Traits.h"
+
+
+class TheFixture : public testing::Test
+{
+};
+
+TEST_F(TheFixture, AddFails)
+{
+	EXPECT_EQ(1000, Add(10, 10));
+}
+
+TEST_F(TheFixture, AddPasses)
+{
+	EXPECT_EQ(20, Add(10, 10));
+}
+
+TEST_F(TheFixture, Crash)
+{
+	int* pInt = NULL;
+	EXPECT_EQ(20, Add(*pInt, 10));
+}
+
+TEST_F_TRAITS1(TheFixture, AddPassesWithTraits, Type, Small)
+{
+	EXPECT_EQ(20, Add(10, 10));
+}
+
+TEST_F_TRAITS2(TheFixture, AddPassesWithTraits2, Type, Small, Author, CSO)
+{
+	EXPECT_EQ(20, Add(10, 10));
+}
+
+TEST_F_TRAITS3(TheFixture, AddPassesWithTraits3, Type, Small, Author, CSO, Category, Integration)
+{
+	EXPECT_EQ(20, Add(10, 10));
+}
+
+
