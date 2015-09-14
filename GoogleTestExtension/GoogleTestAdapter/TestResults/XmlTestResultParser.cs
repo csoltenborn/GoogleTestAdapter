4287f696680e25c3056cc3736dbaa73520654dd3
Christian Soltenborn <christian@soltenborn.de>
2015-09-14 07:59:52 +0200

    added TODOs file added missing files

---
+++ GoogleTestExtension/GoogleTestAdapter/TestResults/XmlTestResultParser.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Xml;
+using System.Linq;
+using System.Globalization;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+
+namespace GoogleTestAdapter
+{
+    public class XmlTestResultParser
+    {
+        private const string ERROR_MSG_NO_XML_FILE = "Output file does not exist, did your tests crash?";
+
+        private static readonly NumberFormatInfo NUMBER_FORMAT_INFO = new CultureInfo("en-US").NumberFormat;
+
+        private readonly IMessageLogger Logger;
+
+        private readonly string XmlResultFile;
+        private readonly List<TestCase> TestCasesRun;
+
+        public XmlTestResultParser(string xmlResultFile, IEnumerable<TestCase> testCases, IMessageLogger logger)
+        {
+            this.Logger = logger;
+            this.XmlResultFile = xmlResultFile;
+            this.TestCasesRun = testCases.ToList();
+        }
+
+        public List<TestResult> GetTestResults()
+        {
+            if (File.Exists(XmlResultFile))
+            {
+                return ParseTestResults();
+            }
+
+            Logger.SendMessage(TestMessageLevel.Warning, ERROR_MSG_NO_XML_FILE);
+            return new List<TestResult>();
+        }
+
+        [SuppressMessage("ReSharper", "PossibleNullReferenceException")]
+        [SuppressMessage("ReSharper", "AssignNullToNotNullAttribute")]
+        private List<TestResult> ParseTestResults()
+        {
+            List<TestResult> TestResults = new List<TestResult>();
+            try
+            {
+                XmlDocument XmlDocument = new XmlDocument();
+                XmlDocument.Load(XmlResultFile);
+                Logger.SendMessage(TestMessageLevel.Informational, "Opened results from " + XmlResultFile);
+
+                XmlNodeList TestsuiteNodes = XmlDocument.DocumentElement.SelectNodes("/testsuites/testsuite");
+                foreach (XmlNode TestsuiteNode in TestsuiteNodes)
+                {
+                    XmlNodeList TestcaseNodes = TestsuiteNode.SelectNodes("testcase");
+                    TestResults.AddRange(TestcaseNodes.Cast<XmlNode>().Select(ParseTestResult).Where(Result => Result != null));
+                }
+            }
+            catch (XmlException e)
+            {
+                Logger.SendMessage(TestMessageLevel.Warning, "Test result file could not be parsed (completely) - your test has probably crashed. Exception message: " + e.Message);
+            }
+            catch (NullReferenceException e)
+            {
+                Logger.SendMessage(TestMessageLevel.Warning, "Test result file could not be parsed (completely) - your test has probably crashed. Exception message: " + e.Message);
+            }
+
+            return TestResults;
+        }
+
+        [SuppressMessage("ReSharper", "PossibleNullReferenceException")]
+        private TestResult ParseTestResult(XmlNode testcaseNode)
+        {
+            string ClassName = testcaseNode.Attributes["classname"].InnerText;
+            string TestCaseName = testcaseNode.Attributes["name"].InnerText;
+            string QualifiedName = ClassName + "." + TestCaseName;
+
+            TestCase TestCase = TestCasesRun.FindTestcase(QualifiedName);
+            if (TestCase == null)
+            {
+                return null;
+            }
+
+            TestResult TestResult = new TestResult(TestCase)
+            {
+                ComputerName = Environment.MachineName,
+                DisplayName = " "
+            };
+
+            string Duration = testcaseNode.Attributes["time"].InnerText;
+            TestResult.Duration = ParseDuration(Duration);
+
+            string TestCaseStatus = testcaseNode.Attributes["status"].InnerText;
+            switch (TestCaseStatus)
+            {
+                case "run":
+                    XmlNodeList FailureNodes = testcaseNode.SelectNodes("failure");
+                    if (FailureNodes.Count == 0)
+                    {
+                        TestResult.Outcome = TestOutcome.Passed;
+                    }
+                    else
+                    {
+                        TestResult.Outcome = TestOutcome.Failed;
+                        TestResult.ErrorMessage = CreateErrorMessage(FailureNodes);
+                    }
+                    break;
+                case "notrun":
+                    TestResult.Outcome = TestOutcome.Skipped;
+                    break;
+                default:
+                    string Msg = "Unknown testcase status: " + TestCaseStatus + ". Please send this information to the developer.";
+                    Logger.SendMessage(TestMessageLevel.Error, Msg);
+                    throw new Exception(Msg);
+            }
+
+            return TestResult;
+        }
+
+        private string CreateErrorMessage(XmlNodeList failureNodes)
+        {
+            IEnumerable<string> ErrorMessages = (from XmlNode failureNode in failureNodes select failureNode.InnerText);
+            return string.Join("\n\n", ErrorMessages);
+        }
+
+        private TimeSpan ParseDuration(string duration)
+        {
+            double Duration = double.Parse(duration, NUMBER_FORMAT_INFO);
+            if (Duration <= 0.001)
+            {
+                Duration = 0.001;
+            }
+            return TimeSpan.FromSeconds(Duration);
+        }
+
+    }
+
+}
---
+++ GoogleTestExtension/GoogleTestAdapterTests/TestResults/XmlTestResultParserTests.cs
@@ -0,0 +1,157 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GoogleTestAdapter
+{
+    [TestClass]
+    public class XmlTestResultParserTests : AbstractGoogleTestExtensionTests
+    {
+
+        private const string XML_FILE_1 = @"..\..\..\testdata\SampleResult1.xml";
+        private const string XML_FILE_2 = @"..\..\..\testdata\SampleResult2.xml";
+        private const string XML_FILE_BROKEN = @"..\..\..\testdata\SampleResult1_Broken.xml";
+
+        [TestMethod]
+        public void FailsNicelyIfFileDoesNotExist()
+        {
+            string[] Tests = new string[] { "BarSuite.BazTest1", "FooSuite.BarTest", "FooSuite.BazTest", "BarSuite.BazTest2" };
+            IEnumerable<TestCase> TestCases = Tests.Select(ToTestCase);
+
+            XmlTestResultParser Parser = new XmlTestResultParser("somefile", TestCases, MockLogger.Object);
+            List<TestResult> Results = Parser.GetTestResults();
+
+            Assert.AreEqual(0, Results.Count);
+            MockLogger.Verify(L => L.SendMessage(It.Is<TestMessageLevel>(Tml => Tml == TestMessageLevel.Warning), It.IsAny<string>()),
+                            Times.Exactly(1));
+        }
+
+        [TestMethod]
+        public void FailsNicelyIfFileIsInvalid()
+        {
+            string[] Tests = new string[] { "GoogleTestSuiteName1.TestMethod_001", "GoogleTestSuiteName1.TestMethod_002" };
+            IEnumerable<TestCase> TestCases = Tests.Select(ToTestCase);
+
+            XmlTestResultParser Parser = new XmlTestResultParser(XML_FILE_BROKEN, TestCases, MockLogger.Object);
+            List<TestResult> Results = Parser.GetTestResults();
+
+            Assert.AreEqual(0, Results.Count);
+            MockLogger.Verify(L => L.SendMessage(It.Is<TestMessageLevel>(Tml => Tml == TestMessageLevel.Warning), It.IsAny<string>()),
+                            Times.Exactly(1));
+        }
+
+        [TestMethod]
+        public void FindsSuccessfulResultsInSample1()
+        {
+            string[] Tests = new string[] { "GoogleTestSuiteName1.TestMethod_001" };
+            IEnumerable<TestCase> TestCases = Tests.Select(ToTestCase);
+
+            XmlTestResultParser Parser = new XmlTestResultParser(XML_FILE_1, TestCases, MockLogger.Object);
+            List<TestResult> Results = Parser.GetTestResults();
+
+            Assert.AreEqual(1, Results.Count);
+            AssertTestResultIsPassed(Results[0]);
+        }
+
+        [TestMethod]
+        public void FindsSuccessfulParameterizedResultInSample1()
+        {
+            string[] Tests = new string[] { "ParameterizedTestsTest1/AllEnabledTest.TestInstance/7  # GetParam() = (false, 200, 0)" };
+            IEnumerable<TestCase> TestCases = Tests.Select(ToTestCase);
+
+            XmlTestResultParser Parser = new XmlTestResultParser(XML_FILE_1, TestCases, MockLogger.Object);
+            List<TestResult> Results = Parser.GetTestResults();
+
+            Assert.AreEqual(1, Results.Count);
+            AssertTestResultIsPassed(Results[0]);
+        }
+
+        [TestMethod]
+        public void FindsFailureResultInSample1()
+        {
+            string[] Tests = new string[] { "AnimalsTest.testGetEnoughAnimals" };
+            IEnumerable<TestCase> TestCases = Tests.Select(ToTestCase);
+
+            XmlTestResultParser Parser = new XmlTestResultParser(XML_FILE_1, TestCases, MockLogger.Object);
+            List<TestResult> Results = Parser.GetTestResults();
+
+            Assert.AreEqual(1, Results.Count);
+            string ErrorMsg = @"x:\prod\company\util\util.cpp:67
+Value of: animals.size()
+  Actual: 1
+Expected: 3
+Should get three animals";
+            AssertTestResultIsFailure(Results[0], ErrorMsg);
+        }
+
+        [TestMethod]
+        public void FindsParamterizedFailureResultInSample1()
+        {
+            string[] Tests = new string[] { "ParameterizedTestsTest1/AllEnabledTest.TestInstance/11  # GetParam() = (true, 0, 100)" };
+            IEnumerable<TestCase> TestCases = Tests.Select(ToTestCase);
+
+            XmlTestResultParser Parser = new XmlTestResultParser(XML_FILE_1, TestCases, MockLogger.Object);
+            List<TestResult> Results = Parser.GetTestResults();
+
+            Assert.AreEqual(1, Results.Count);
+            string ErrorMsg = @"someSimpleParameterizedTest.cpp:61
+Expected: (0) != ((pGSD->g_outputs64[(g_nOutput[ 8 ]-1)/64] & g_dnOutput[g_nOutput[ 8 ]])), actual: 0 vs 0";
+            AssertTestResultIsFailure(Results[0], ErrorMsg);
+        }
+
+        [TestMethod]
+        public void FindsSuccessfulResultInSample2()
+        {
+            string[] Tests = new string[] { "FooTest.DoesXyz" };
+            IEnumerable<TestCase> TestCases = Tests.Select(ToTestCase);
+
+            XmlTestResultParser Parser = new XmlTestResultParser(XML_FILE_2, TestCases, MockLogger.Object);
+            List<TestResult> Results = Parser.GetTestResults();
+
+            Assert.AreEqual(1, Results.Count);
+            AssertTestResultIsPassed(Results[0]);
+        }
+
+        [TestMethod]
+        public void FindsFailureResultInSample2()
+        {
+            string[] Tests = new string[] { "FooTest.MethodBarDoesAbc" };
+            IEnumerable<TestCase> TestCases = Tests.Select(ToTestCase);
+
+            XmlTestResultParser Parser = new XmlTestResultParser(XML_FILE_2, TestCases, MockLogger.Object);
+            List<TestResult> Results = Parser.GetTestResults();
+
+            Assert.AreEqual(1, Results.Count);
+            string ErrorMsg = @"c:\prod\gtest-1.7.0\staticallylinkedgoogletests\main.cpp:40
+Value of: output_filepath
+  Actual: ""this/package/testdata/myoutputfile.dat""
+Expected: input_filepath
+Which is: ""this/package/testdata/myinputfile.dat""
+Something's not right!!
+
+c:\prod\gtest-1.7.0\staticallylinkedgoogletests\main.cpp:41
+Value of: 56456
+Expected: 12312
+Something's wrong :(";
+            AssertTestResultIsFailure(Results[0], ErrorMsg);
+        }
+
+        private void AssertTestResultIsPassed(TestResult testResult)
+        {
+            Assert.AreEqual(TestOutcome.Passed, testResult.Outcome);
+            Assert.IsNull(testResult.ErrorMessage);
+        }
+
+        private void AssertTestResultIsFailure(TestResult testResult, string errorMessage)
+        {
+            Assert.AreEqual(TestOutcome.Failed, testResult.Outcome);
+            Assert.IsNotNull(testResult.ErrorMessage);
+            Assert.AreEqual(errorMessage.Replace("\r\n", "\n"), testResult.ErrorMessage.Replace("\r\n", "\n"));
+        }
+
+    }
+
+}
