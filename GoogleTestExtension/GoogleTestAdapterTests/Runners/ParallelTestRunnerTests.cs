1440951679cf6e23a034c63a23170a4cfd9b6f5f
Christian Soltenborn <christian@soltenborn.de>
2015-10-25 09:14:19 +0100

    trying to improve test stability when computing test coverage

--- GoogleTestExtension/GoogleTestAdapterTests/Runners/ParallelTestRunnerTests.cs
+++ GoogleTestExtension/GoogleTestAdapterTests/Runners/ParallelTestRunnerTests.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using GoogleTestAdapter.Helpers;
+using System.Collections.Generic;
 
 namespace GoogleTestAdapter.Runners
 {
@@ -18,24 +19,26 @@ namespace GoogleTestAdapter.Runners
             }
 
             Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
             GoogleTestExecutor executor = new GoogleTestExecutor(TestEnvironment);
-            executor.RunTests(SampleTests.Yield(), MockRunContext.Object, MockFrameworkHandle.Object);
+            IEnumerable<string> testsToRun = SampleTests.Yield();
+            stopwatch.Start();
+            executor.RunTests(testsToRun, MockRunContext.Object, MockFrameworkHandle.Object);
             stopwatch.Stop();
             long sequentialDuration = stopwatch.ElapsedMilliseconds;
 
             MockOptions.Setup(o => o.ParallelTestExecution).Returns(true);
             MockOptions.Setup(o => o.MaxNrOfThreads).Returns(Environment.ProcessorCount);
 
-            stopwatch.Restart();
             executor = new GoogleTestExecutor(TestEnvironment);
-            executor.RunTests(SampleTests.Yield(), MockRunContext.Object, MockFrameworkHandle.Object);
+            testsToRun = SampleTests.Yield();
+            stopwatch.Restart();
+            executor.RunTests(testsToRun, MockRunContext.Object, MockFrameworkHandle.Object);
             stopwatch.Stop();
             long parallelDuration = stopwatch.ElapsedMilliseconds;
 
             Assert.IsTrue(sequentialDuration > 4000, sequentialDuration.ToString()); // 2 long tests, 2 seconds per test
             Assert.IsTrue(parallelDuration > 2000, parallelDuration.ToString());
-            Assert.IsTrue(parallelDuration < 3000, parallelDuration.ToString()); // 2 seconds per long test + some time for the rest
+            Assert.IsTrue(parallelDuration < 3200, parallelDuration.ToString()); // 2 seconds per long test + some time for the rest
         }
 
     }
