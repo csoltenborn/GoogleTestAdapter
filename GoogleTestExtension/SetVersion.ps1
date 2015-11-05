bf33ab437f3da37f279d94c54eb0eb8dcf325b75
Christian Soltenborn <christian@soltenborn.de>
2015-11-05 20:07:31 +0100

    fixed version reference

--- GoogleTestExtension/SetVersion.ps1
+++ GoogleTestExtension/SetVersion.ps1
@@ -6,6 +6,6 @@ $vsix_assembly_info = "GoogleTestAdapterVSIX\Properties\AssemblyInfo.cs"
 $vsix_manifest = "GoogleTestAdapterVSIX\source.extension.vsixmanifest"
 
 (Get-Content $gta_assembly_info) | ForEach-Object { $_ -replace "0.1.0.0", $version } | Set-Content $gta_assembly_info
-(Get-Content $test_assembly_info) | ForEach-Object { $_ -replace "0.1.0.0", $theversion } | Set-Content $test_assembly_info
+(Get-Content $test_assembly_info) | ForEach-Object { $_ -replace "0.1.0.0", $version } | Set-Content $test_assembly_info
 (Get-Content $vsix_assembly_info) | ForEach-Object { $_ -replace "0.1.0.0", $version } | Set-Content $vsix_assembly_info
 (Get-Content $vsix_manifest) | ForEach-Object { $_ -replace "0.1.0.0", $version } | Set-Content $vsix_manifest
