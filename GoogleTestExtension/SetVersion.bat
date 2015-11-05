a6205827277cf8ceac2b86e4f5c007d465e331f1
Christian Soltenborn <christian@soltenborn.de>
2015-11-05 19:59:38 +0100

    corrected stupid error  in btach file

--- GoogleTestExtension/SetVersion.bat
+++ GoogleTestExtension/SetVersion.bat
@@ -1,8 +1,10 @@
-@echo off
+@echo on
 
 if "%1"=="" goto _USAGE
 
+echo Setting versions to %1
 PowerShell.exe -ExecutionPolicy Bypass -File "SetVersion.ps1" -version "%1"
+goto :eof
 
 :_USAGE
 echo Usage  : SetVersion ^<version^>, where ^<version^> should be ^<major^>.^<minor^>.^<revision^>.^<build^>
