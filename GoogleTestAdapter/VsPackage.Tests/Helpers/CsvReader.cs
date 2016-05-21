71e4f16a633248c6a41d3040774d79a25a5bf2e4
Christian Soltenborn <christian@soltenborn.de>
2016-05-21 15:04:12 +0200

    minor code generation improvements

--- GoogleTestAdapter/VsPackage.Tests/Helpers/CsvReader.cs
+++ GoogleTestAdapter/VsPackage.Tests/Helpers/CsvReader.cs
@@ -6,34 +6,33 @@ namespace GoogleTestAdapter.VsPackage.Helpers
 {
     public abstract class CsvReader<T>
     {
+        private readonly string _csvFile;
+        private readonly char _separator;
+        private readonly int _firstDataRow;
 
-        private readonly string csvFile;
-        private readonly char separator;
-        private readonly int firstDataRow;
-
-        public CsvReader(string csvFile, char separator, bool header)
+        protected CsvReader(string csvFile, char separator, bool header)
         {
             if (!File.Exists(csvFile))
                 throw new ArgumentException($"File does not exist: {csvFile}", nameof(csvFile));
 
-            this.csvFile = csvFile;
-            this.separator = separator;
-            firstDataRow = header ? 1 : 0;
+            _csvFile = csvFile;
+            _separator = separator;
+            _firstDataRow = header ? 1 : 0;
         }
+
+        protected abstract T GetObject(string[] columns);
 
         public List<T> GetObjects()
         {
             List<T> result = new List<T>();
-            string[] lines = File.ReadAllLines(csvFile);
-            for (int i = firstDataRow; i < lines.Length; i++)
+            string[] lines = File.ReadAllLines(_csvFile);
+            for (int i = _firstDataRow; i < lines.Length; i++)
             {
-                string[] columns = lines[i].Split(separator);
+                string[] columns = lines[i].Split(_separator);
                 result.Add(GetObject(columns));
             }
             return result;
         }
-
-        protected abstract T GetObject(string[] columns);
 
     }
 
