7018123b2f2edfb1c5ed10e56e563e1bd63aeafd
Christian Soltenborn <christian@soltenborn.de>
2017-03-12 16:23:05 +0100

    stabilizing timing based test

--- GoogleTestAdapter/TestAdapter.Tests/TestExecutorSequentialTests.cs
+++ GoogleTestAdapter/TestAdapter.Tests/TestExecutorSequentialTests.cs
@@ -16,7 +16,7 @@ namespace GoogleTestAdapter.TestAdapter
     {
         private const int DurationOfEachLongRunningTestInMs = 2000;
         private const int WaitBeforeCancelInMs = 1000;
-        private static readonly int OverheadInMs = !CiSupport.IsRunningOnBuildServer ? 500 : 1500;
+        private static readonly int OverheadInMs = !CiSupport.IsRunningOnBuildServer ? 500 : 1600;
 
         public TestExecutorSequentialTests() : base(false, 1) { }
 
