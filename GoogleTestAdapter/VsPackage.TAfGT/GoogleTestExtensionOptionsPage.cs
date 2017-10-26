22b124d4d76cc5a1c077186eb07345597f7c6467
Spencer Bloom <spebl@microsoft.com>
2017-10-26 16:33:55 -0700

    update ids to not clash with stand alone version (#80)

--- GoogleTestAdapter/VsPackage.TAfGT/GoogleTestExtensionOptionsPage.cs
+++ GoogleTestAdapter/VsPackage.TAfGT/GoogleTestExtensionOptionsPage.cs
@@ -12,7 +12,7 @@ namespace GoogleTestAdapter.VsPackage
         new string[] { VSConstants.UICONTEXT.VCProject_string, TestExplorerContextGuid })]
     public partial class GoogleTestExtensionOptionsPage
     {
-        private const string PackageGuidString = "1db31773-234b-424b-a887-b451fb1ba837";
+        private const string PackageGuidString = "6fac3232-df1d-400a-95ac-7daeaaee74ac";
         private const string UIContextGuid = "7517f9ae-397f-48e1-8e1b-dac609d9f52d";
         private const string TestExplorerContextGuid = "ec25b527-d893-4ec0-a814-d2c9f1782997";
         private const string OptionsCategoryName = "Test Adapter for Google Test";
