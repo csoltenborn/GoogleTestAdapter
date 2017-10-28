a76e861eb0dbefb1f201b7c7898a5648225d8bf6
Christian Soltenborn <christian@soltenborn.de>
2017-10-28 14:15:21 +0200

    added debug output to build preparation script

--- build_preparation.bat
+++ build_preparation.bat
@@ -6,6 +6,8 @@ set DIA_SDK="%VS_LOCATION%\DIA SDK\bin"
 set VC_VARS_BAT="%VS_LOCATION%\VC\Auxiliary\Build\vcvars32.bat"
 set MS_BUILD="%VS_LOCATION%\MSBuild\15.0\Bin\MSBuild.exe"
 
+echo Platform: %PLATFORM%
+set TEMP_PLATFORM=%PLATFORM%
 
 if defined APPVEYOR goto Build
 
@@ -28,29 +30,36 @@ if not "%input%" == "yes" goto End
 
 echo Setting adapter flavor to GTA
 powershell -Command "(gc TestAdapterFlavor.props) -replace '>TAfGT<', '>GTA<' | Out-File TestAdapterFlavor.props"
+echo Platform: %PLATFORM%
 
 echo Executing T4 scripts
 set VisualStudioVersion=15.0
 %MS_BUILD% ResolveTTs.proj
+echo Platform: %PLATFORM%
 
 echo Removing TAfGT projects (for now)
 powershell -ExecutionPolicy Bypass .\Tools\RemoveProjects.ps1 -flavor GTA
+echo Platform: %PLATFORM%
 
 echo Restoring NuGet packages
 cd GoogleTestAdapter
 nuget restore
+echo Platform: %PLATFORM%
 
 echo Setting up VS Developer Prompt environment
 call %VC_VARS_BAT%
+echo Platform: %PLATFORM%
 
 echo Copying DIA dlls
 cd DiaResolver
 copy %DIA_SDK%\msdia140.dll x86
 copy %DIA_SDK%\amd64\msdia140.dll x64
+echo Platform: %PLATFORM%
 
 echo Generating dia2.dll
 cd dia2
 powershell -ExecutionPolicy Bypass .\compile_typelib.ps1
+echo Platform: %PLATFORM%
 
 echo NOT building Google Test NuGet packages
 cd ..\..\..
@@ -63,6 +72,12 @@ rem git submodule update
 rem cd GoogleTestNuGet
 rem powershell .\Build.ps1 -Verbose
 rem cd ..
+
+
+if not defined APPVEYOR goto End
+
+set Platform=%TEMP_PLATFORM%
+echo Platform: %PLATFORM%
 
 
 :End
