5aa605157b2f854c92264c6d28f80fbe471b23aa
Christian Soltenborn <christian@soltenborn.de>
2018-10-07 09:48:52 +0200

    updated SampleTests solution to use gtest 1.8.1

--- GoogleTestAdapter/Tests.Common/Tests/ProcessExecutorTests.cs
+++ GoogleTestAdapter/Tests.Common/Tests/ProcessExecutorTests.cs
@@ -46,7 +46,7 @@ namespace GoogleTestAdapter.Tests.Common.Tests
 
             exitCode.Should().Be(1);
             output.Should().Contain(s => s.Contains("TestMath.AddPasses"));
-            output.Should().HaveCount(563);
+            output.Should().HaveCount(608);
         }
 
         protected void Test_WithSimpleCommand_ReturnsOutputOfCommand()
