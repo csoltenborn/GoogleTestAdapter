d10195d09efc033ce0ef5ebf0a233fe2aa816633
Christian Soltenborn <christian@soltenborn.de>
2019-04-28 15:11:49 +0200

    added version, fixed button width

--- GoogleTestAdapter/VsPackage.GTA/ReleaseNotes/ReleaseNotesDialog.Designer.cs
+++ GoogleTestAdapter/VsPackage.GTA/ReleaseNotes/ReleaseNotesDialog.Designer.cs
@@ -33,17 +33,17 @@ namespace GoogleTestAdapter.VsPackage.ReleaseNotes
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new Container();
-            ComponentResourceManager resources = new ComponentResourceManager(typeof(ReleaseNotesDialog));
-            this.RootPanel = new TableLayoutPanel();
-            this.NavigationPanel = new FlowLayoutPanel();
-            this.BackButton = new Button();
-            this.ForwardButton = new Button();
-            this.WebBrowser = new WebBrowser();
-            this.OkButtonPanel = new TableLayoutPanel();
-            this.DonateButton = new Button();
-            this.OkButton = new Button();
-            this.toolTip = new ToolTip(this.components);
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReleaseNotesDialog));
+            this.RootPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.NavigationPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.BackButton = new System.Windows.Forms.Button();
+            this.ForwardButton = new System.Windows.Forms.Button();
+            this.WebBrowser = new System.Windows.Forms.WebBrowser();
+            this.OkButtonPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.DonateButton = new System.Windows.Forms.Button();
+            this.OkButton = new System.Windows.Forms.Button();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
             this.RootPanel.SuspendLayout();
             this.NavigationPanel.SuspendLayout();
             this.OkButtonPanel.SuspendLayout();
@@ -52,21 +52,21 @@ namespace GoogleTestAdapter.VsPackage.ReleaseNotes
             // RootPanel
             // 
             this.RootPanel.ColumnCount = 2;
-            this.RootPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
-            this.RootPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            this.RootPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.RootPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.RootPanel.Controls.Add(this.NavigationPanel, 0, 0);
             this.RootPanel.Controls.Add(this.WebBrowser, 0, 1);
             this.RootPanel.Controls.Add(this.OkButtonPanel, 0, 2);
-            this.RootPanel.Dock = DockStyle.Fill;
-            this.RootPanel.Location = new Point(0, 0);
-            this.RootPanel.Margin = new Padding(7);
+            this.RootPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.RootPanel.Location = new System.Drawing.Point(0, 0);
+            this.RootPanel.Margin = new System.Windows.Forms.Padding(7);
             this.RootPanel.Name = "RootPanel";
             this.RootPanel.RowCount = 3;
-            this.RootPanel.RowStyles.Add(new RowStyle());
-            this.RootPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            this.RootPanel.RowStyles.Add(new RowStyle());
-            this.RootPanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 45F));
-            this.RootPanel.Size = new Size(1829, 1251);
+            this.RootPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.RootPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.RootPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.RootPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 45F));
+            this.RootPanel.Size = new System.Drawing.Size(1829, 1251);
             this.RootPanel.TabIndex = 0;
             // 
             // NavigationPanel
@@ -75,21 +75,21 @@ namespace GoogleTestAdapter.VsPackage.ReleaseNotes
             this.RootPanel.SetColumnSpan(this.NavigationPanel, 2);
             this.NavigationPanel.Controls.Add(this.BackButton);
             this.NavigationPanel.Controls.Add(this.ForwardButton);
-            this.NavigationPanel.Dock = DockStyle.Fill;
-            this.NavigationPanel.Location = new Point(7, 7);
-            this.NavigationPanel.Margin = new Padding(7);
+            this.NavigationPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.NavigationPanel.Location = new System.Drawing.Point(7, 7);
+            this.NavigationPanel.Margin = new System.Windows.Forms.Padding(7);
             this.NavigationPanel.Name = "NavigationPanel";
-            this.NavigationPanel.Size = new Size(1815, 65);
+            this.NavigationPanel.Size = new System.Drawing.Size(1815, 65);
             this.NavigationPanel.TabIndex = 3;
             // 
             // BackButton
             // 
             this.BackButton.AccessibleName = "Go back";
             this.BackButton.Enabled = false;
-            this.BackButton.Location = new Point(7, 7);
-            this.BackButton.Margin = new Padding(7);
+            this.BackButton.Location = new System.Drawing.Point(7, 7);
+            this.BackButton.Margin = new System.Windows.Forms.Padding(7);
             this.BackButton.Name = "BackButton";
-            this.BackButton.Size = new Size(54, 51);
+            this.BackButton.Size = new System.Drawing.Size(54, 51);
             this.BackButton.TabIndex = 3;
             this.BackButton.Text = "<";
             this.BackButton.UseVisualStyleBackColor = true;
@@ -98,10 +98,10 @@ namespace GoogleTestAdapter.VsPackage.ReleaseNotes
             // 
             this.ForwardButton.AccessibleName = "Go forward";
             this.ForwardButton.Enabled = false;
-            this.ForwardButton.Location = new Point(75, 7);
-            this.ForwardButton.Margin = new Padding(7);
+            this.ForwardButton.Location = new System.Drawing.Point(75, 7);
+            this.ForwardButton.Margin = new System.Windows.Forms.Padding(7);
             this.ForwardButton.Name = "ForwardButton";
-            this.ForwardButton.Size = new Size(54, 51);
+            this.ForwardButton.Size = new System.Drawing.Size(54, 51);
             this.ForwardButton.TabIndex = 4;
             this.ForwardButton.Text = ">";
             this.ForwardButton.UseVisualStyleBackColor = true;
@@ -111,44 +111,44 @@ namespace GoogleTestAdapter.VsPackage.ReleaseNotes
             this.WebBrowser.AccessibleName = "Browser";
             this.WebBrowser.AllowWebBrowserDrop = false;
             this.RootPanel.SetColumnSpan(this.WebBrowser, 2);
-            this.WebBrowser.Dock = DockStyle.Fill;
+            this.WebBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
             this.WebBrowser.IsWebBrowserContextMenuEnabled = false;
-            this.WebBrowser.Location = new Point(7, 86);
-            this.WebBrowser.Margin = new Padding(7);
-            this.WebBrowser.MinimumSize = new Size(47, 45);
+            this.WebBrowser.Location = new System.Drawing.Point(7, 86);
+            this.WebBrowser.Margin = new System.Windows.Forms.Padding(7);
+            this.WebBrowser.MinimumSize = new System.Drawing.Size(47, 45);
             this.WebBrowser.Name = "WebBrowser";
             this.WebBrowser.ScriptErrorsSuppressed = true;
-            this.WebBrowser.Size = new Size(1815, 1079);
+            this.WebBrowser.Size = new System.Drawing.Size(1815, 1079);
             this.WebBrowser.TabIndex = 2;
             this.WebBrowser.WebBrowserShortcutsEnabled = false;
-            this.WebBrowser.Navigating += new WebBrowserNavigatingEventHandler(this.WebBrowser_Navigating);
+            this.WebBrowser.Navigating += new System.Windows.Forms.WebBrowserNavigatingEventHandler(this.WebBrowser_Navigating);
             // 
             // OkButtonPanel
             // 
-            this.OkButtonPanel.Anchor = ((AnchorStyles)((AnchorStyles.Left | AnchorStyles.Right)));
+            this.OkButtonPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.OkButtonPanel.AutoSize = true;
             this.OkButtonPanel.ColumnCount = 2;
             this.RootPanel.SetColumnSpan(this.OkButtonPanel, 2);
-            this.OkButtonPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.33333F));
-            this.OkButtonPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.33333F));
+            this.OkButtonPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.OkButtonPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.OkButtonPanel.Controls.Add(this.DonateButton, 0, 0);
             this.OkButtonPanel.Controls.Add(this.OkButton, 1, 0);
-            this.OkButtonPanel.Location = new Point(7, 1179);
-            this.OkButtonPanel.Margin = new Padding(7);
+            this.OkButtonPanel.Location = new System.Drawing.Point(7, 1179);
+            this.OkButtonPanel.Margin = new System.Windows.Forms.Padding(7);
             this.OkButtonPanel.Name = "OkButtonPanel";
             this.OkButtonPanel.RowCount = 1;
-            this.OkButtonPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            this.OkButtonPanel.Size = new Size(1815, 65);
+            this.OkButtonPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.OkButtonPanel.Size = new System.Drawing.Size(1815, 65);
             this.OkButtonPanel.TabIndex = 4;
             // 
             // DonateButton
             // 
             this.DonateButton.AccessibleName = "Close release notes dialog";
-            this.DonateButton.Anchor = AnchorStyles.None;
-            this.DonateButton.Location = new Point(303, 7);
-            this.DonateButton.Margin = new Padding(7);
+            this.DonateButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.DonateButton.Location = new System.Drawing.Point(303, 7);
+            this.DonateButton.Margin = new System.Windows.Forms.Padding(7);
             this.DonateButton.Name = "DonateButton";
-            this.DonateButton.Size = new Size(301, 51);
+            this.DonateButton.Size = new System.Drawing.Size(301, 51);
             this.DonateButton.TabIndex = 0;
             this.DonateButton.Text = "Cool - I want to donate!";
             this.DonateButton.UseVisualStyleBackColor = true;
@@ -156,12 +156,12 @@ namespace GoogleTestAdapter.VsPackage.ReleaseNotes
             // OkButton
             // 
             this.OkButton.AccessibleName = "Close release notes dialog";
-            this.OkButton.Anchor = AnchorStyles.None;
-            this.OkButton.DialogResult = DialogResult.Cancel;
-            this.OkButton.Location = new Point(1273, 7);
-            this.OkButton.Margin = new Padding(7);
+            this.OkButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.OkButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.OkButton.Location = new System.Drawing.Point(1231, 7);
+            this.OkButton.Margin = new System.Windows.Forms.Padding(7);
             this.OkButton.Name = "OkButton";
-            this.OkButton.Size = new Size(175, 51);
+            this.OkButton.Size = new System.Drawing.Size(260, 51);
             this.OkButton.TabIndex = 1;
             this.OkButton.Text = "Thanks anyways...";
             this.OkButton.UseVisualStyleBackColor = true;
@@ -170,16 +170,16 @@ namespace GoogleTestAdapter.VsPackage.ReleaseNotes
             // 
             this.AcceptButton = this.OkButton;
             this.AccessibleName = "Release notes of Google Test Adapter";
-            this.AutoScaleDimensions = new SizeF(14F, 29F);
-            this.AutoScaleMode = AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.OkButton;
             this.CausesValidation = false;
-            this.ClientSize = new Size(1829, 1251);
+            this.ClientSize = new System.Drawing.Size(1829, 1251);
             this.Controls.Add(this.RootPanel);
-            this.Icon = ((Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new Padding(7);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(7);
             this.Name = "ReleaseNotesDialog";
-            this.StartPosition = FormStartPosition.CenterParent;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Google Test Adapter: Release notes";
             this.RootPanel.ResumeLayout(false);
             this.RootPanel.PerformLayout();
