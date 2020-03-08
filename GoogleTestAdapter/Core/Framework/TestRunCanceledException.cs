9996a0be59c24623bd453ca6d8b8de83f71c329b
Christian Soltenborn <christian@soltenborn.de>
2020-03-08 15:22:48 +0100

    fixed warning

--- GoogleTestAdapter/Core/Framework/TestRunCanceledException.cs
+++ GoogleTestAdapter/Core/Framework/TestRunCanceledException.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace GoogleTestAdapter.Framework
 {
     [Serializable]
     public sealed class TestRunCanceledException : Exception
     {
-        public TestRunCanceledException(string message, Exception innerException) : base(message, innerException) { }
+        public TestRunCanceledException(string message, Exception innerException) 
+            : base(message, innerException) { }
+
+        private TestRunCanceledException(SerializationInfo serializationInfo, StreamingContext streamingContext)
+            : base(serializationInfo, streamingContext) { }
     }
 }
